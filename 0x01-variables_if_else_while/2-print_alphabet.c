f5a95ff6059ebfd02223b17b71daeda87b899d07
kebshaaa <kamanuel61@gmail.com>
2023-08-12 03:29:43 -0700

    selam seten

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- * Description: 'print alphabets lowercas'
- * Return: Always 0
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n = 97;
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-	while (n <= 122)
+	for (i = 0; i < 26; i++)
 	{
-		putcher(n);
-		n++;
+		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);
